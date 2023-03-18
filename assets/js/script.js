6eb193f6e3c360274a8c6e08e28c7617557b6f3a
Nicowski25 <nicola.faedo0610@gmail.com>
2023-03-18 17:12:27 +0100

    for that adds white class

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,7 @@
 const playBtn = document.getElementById('playbtn')
 console.log(playBtn);
 
-//identifichiamo il form-select e prendiamo il valore che inserisce l'utente
-let difficultyInput = document.getElementById('difficulty').value
-console.log(difficultyInput);
-
+//funzione per definire la difficoltà e le caselle relative ad essa
 function setDifficulty(value) {
     if (value == 1) {
         return 49
@@ -19,16 +16,34 @@ function setDifficulty(value) {
 }
 
 //creiamo la funzione onclick
-function game(boxSize) {
+function game() {
+    //identifichiamo il form-select e prendiamo il valore che inserisce l'utente
+    let difficultyInput = document.getElementById('difficulty').value
+    const boxSize = setDifficulty(difficultyInput);
     //indentifico il container dalla dom
     const container = document.querySelector('.container')
+    container.innerHTML = ''
     for (let i = 1; i <= boxSize; i++) {
         //impostiamo le box da appendere
         const boxCreate = `<div class="box"> ${i} </div>`
         //inseriamo le box nel container
-        container.innerHTML += boxCreate
-        console.log(i);    
+        container.innerHTML += boxCreate 
     }
 }
 
-playBtn.addEventListener("click", game(setDifficulty(difficultyInput)))
+playBtn.addEventListener("click", game())
+
+const boxEl = document.querySelectorAll('div.box')
+console.log(boxEl);
+
+for (let i = 0; i < boxEl.length; i++) {
+    const thisBox = boxEl[i];
+    console.log(thisBox);
+    thisBox.addEventListener('click', function() {
+        thisBox.classList.add('white')
+    }
+    )
+}
+
+
+
